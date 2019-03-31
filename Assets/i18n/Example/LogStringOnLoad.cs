34071329be6ee266cc7e8e417b7f0baf90247748
Adam Passey <adamjpassey@gmail.com>
2019-03-31 10:28:11 -0700

    Adding example

---
+++ Assets/i18n/Example/LogStringOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogStringOnLoad : MonoBehaviour
+{
+    public string Key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LogTestString", 1f);
+    }
+
+    public void LogTestString() {
+        Debug.Log(i18n.Strings.Get("hello", "Adam"));
+    }
+}
