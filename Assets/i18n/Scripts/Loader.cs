1fe196c02edc299e2e82884ee4594777894f8269
Adam Passey <adamjpassey@gmail.com>
2019-03-31 10:22:20 -0700

    Initial commit

---
+++ Assets/i18n/Scripts/Loader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace i18n {
+
+    public class Loader : MonoBehaviour {
+
+        public string LangDirectory = "Lang";
+        public string DefaultLang = "en-US";
+
+        [System.Serializable]
+        private class LocalizationData {
+            public LocalizationItem[] strings;
+        }
+
+        [System.Serializable]
+        private class LocalizationItem {
+            public string key;
+            public string value;
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+            Load(DefaultLang, LangDirectory);
+        }
+
+        public void Load(string lang, string directory) {
+            string path = Path.Combine(Application.streamingAssetsPath, directory, $"{lang}.json");
+            if (File.Exists(path)) {
+                string jsonData = File.ReadAllText(path);
+                LocalizationData localizationData = JsonUtility.FromJson<LocalizationData>(jsonData);
+                Parse(localizationData);
+            } else {
+                Debug.LogError($"Unable to load lang file at path ${path}");
+            }
+        }
+
+        private void Parse(LocalizationData data) {
+            Dictionary<string, string> keys = new Dictionary<string, string>();
+
+            for (int i = 0; i < data.strings.Length; i++) {
+                keys.Add(data.strings[i].key, data.strings[i].value);
+            }
+
+            Strings.LoadStrings(keys);
+        }
+    }
+}
---
+++ Assets/i18n/Scripts/Strings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i18n {
+    public static class Strings {
+
+        private static Dictionary<string, string> strings;
+
+        public static void LoadStrings(Dictionary<string, string> s) {
+            strings = s;
+        }
+
+        public static void AddString(string key, string value) {
+            strings.Add(key, value);
+        }
+
+        public static string Get(string key, params string[] p) {
+            strings.TryGetValue(key, out string result);
+
+            return string.Format(result, p);
+        }
+    }
+}
